8b986bd5368d99f34f04471dcbf1c31d9ac51187
Makino Kengo <ognek.m@gmail.com>
2020-06-24 22:54:35 +0900

    定数を命名規約に沿うように変更

--- thingspeak.py
+++ thingspeak.py
@@ -1,14 +1,18 @@
 #coding: utf-8
-
+"""
+Thingspeakの情報を読み出す関数群
+"""
 import os
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import json
 
-my_thingspeak_apikey_sensor = os.getenv('THINGSPEAK_APIKEY_SENSOR', None)
-my_thingspeak_channel_sensor = os.getenv('THINGSPEAK_CHANNEL_SENSOR', None)
+MY_THINGSPEAK_APIKEY_SENSOR = os.getenv('THINGSPEAK_APIKEY_SENSOR', None)
+MY_THINGSPEAK_CHANNEL_SENSOR = os.getenv('THINGSPEAK_CHANNEL_SENSOR', None)
 
-my_thingspeak_apikey_state = os.getenv('THINGSPEAK_APIKEY_STATE', None)
-my_thingspeak_channel_state = os.getenv('THINGSPEAK_CHANNEL_STATE', None)
+MY_THINGSPEAK_APIKEY_STATE = os.getenv('THINGSPEAK_APIKEY_STATE', None)
+MY_THINGSPEAK_CHANNEL_STATE = os.getenv('THINGSPEAK_CHANNEL_STATE', None)
 
 url_template_write = "https://api.thingspeak.com/update?api_key={api_key}&field1={state}"
 url_template_read = "https://api.thingspeak.com/channels/{channel}/feeds.json?api_key={api_key}&results={num_result}"
@@ -17,7 +21,7 @@ num_result = 1
 url_template = "https://api.thingspeak.com/channels/{channel}/feeds.json?api_key={api_key}&results={num_result}&timezone=Asia/Tokyo"
 
 def get_environment_field():
-    url = url_template.format(channel=my_thingspeak_channel_sensor, api_key=my_thingspeak_apikey_sensor, num_result=num_result)
+    url = url_template.format(channel=MY_THINGSPEAK_CHANNEL_SENSOR, api_key=MY_THINGSPEAK_APIKEY_SENSOR, num_result=num_result)
     result = urllib.request.urlopen(url).read()
     data = json.loads(result)
     feeds = data.get("feeds")
@@ -39,7 +43,7 @@ def get_environment_field():
 
 def read_current_state():
     num_result = 1
-    url = url_template_read.format(channel=my_thingspeak_channel_state, api_key=my_thingspeak_apikey_state, num_result=num_result)
+    url = url_template_read.format(channel=MY_THINGSPEAK_CHANNEL_STATE, api_key=MY_THINGSPEAK_APIKEY_STATE, num_result=num_result)
     result = urllib.request.urlopen(url).read()
     data = json.loads( result )
     feeds = data.get("feeds")
@@ -47,7 +51,7 @@ def read_current_state():
     return latest_feed.get("field1")
 
 def write_current_state(state_to_write):
-    url = url_template_write.format(api_key=my_thingspeak_apikey_state, state=state_to_write)
+    url = url_template_write.format(api_key=MY_THINGSPEAK_APIKEY_STATE, state=state_to_write)
     result = urllib.request.urlopen(url).read()
 
 if __name__ == "__main__":
